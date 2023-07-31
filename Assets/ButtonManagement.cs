0700e4cf0bed391b7184173ba95ad8a12774afb4
AurelianApet <fastmoon66@outlook.com>
2023-07-31 17:54:45 +0900

    last version

---
+++ Assets/ButtonManagement.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ButtonManagement : MonoBehaviour
+{
+    // Start is called before the first frame update
+
+    public GameObject btnClose;
+    public GameObject btnOk;
+    public GameObject btnCancel;
+    void Start()
+    {
+        
+    }
+    public void appClose()
+    {
+        Debug.Log("App close Click!");
+        btnClose.SetActive(true);
+    }
+    public void clickOkBtn()
+    {
+        Application.Quit();
+    }
+    public void clickCancelBtn()
+    {
+        btnClose.SetActive(false);
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
